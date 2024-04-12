786478ac2e72caf1e7d3cbc406b11351b5269615
todokaima <146889336+todokaima@users.noreply.github.com>
2024-04-12 19:47:38 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\"\tHello\t world!\t\"");
+        System.out.print("Hello world!");
+        System.out.print( );
+    }
+}
